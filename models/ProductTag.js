cc879b2a9ff298aa582c5e01e3fcb0bfe223bac9
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-06-08 12:46:00 -0500

    added id of tag_id

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -23,13 +23,7 @@ ProductTag.init(
       }
     },
 
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "tag",
-        key: "id",
-      }
-    }
+    
   },
   {
     sequelize,
