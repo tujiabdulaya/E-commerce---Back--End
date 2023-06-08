79b4703bedc00da64887dce6f4e5a0d109a85db3
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-06-08 11:56:28 -0500

    added id in category

--- models/Category.js
+++ models/Category.js
@@ -7,13 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true, 
-      allowNull: false
-
-    },
+    
 
     category_name: {
       type: DataTypes.STRING,
