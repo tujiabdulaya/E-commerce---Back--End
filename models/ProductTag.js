fd4ead1b169bcafe82e2acac44a7f72570d841e3
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-06-08 12:45:14 -0500

    added id of productID

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,14 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
-    
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, 
+      allowNull: false,
+
+    },
+
     product_id: {
       type: DataTypes.INTEGER,
       references: {
