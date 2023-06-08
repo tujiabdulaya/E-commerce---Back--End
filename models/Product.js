4c18f4b045a4abcd846532f448f747017a8790e9
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-06-08 12:42:44 -0500

    added Category_id

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,14 @@ Product.init(
       }
     },
 
-    
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
